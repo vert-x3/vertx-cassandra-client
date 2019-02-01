165ee2ccc3369bda19f580fe2edefa146097882c
Thomas Segismont <tsegismo@redhat.com>
2019-02-01 16:37:19 +0100

    Fixes #26 Simplify and accelerate test suite

---
+++ src/test/java/io/vertx/cassandra/CassandraClientTestBase.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.vertx.cassandra;
+
+import com.datastax.driver.core.Row;
+import io.vertx.core.*;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.cassandraunit.CQLDataLoader;
+import org.cassandraunit.dataset.CQLDataSet;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author Pavel Drankou
+ * @author Thomas Segismont
+ */
+@RunWith(VertxUnitRunner.class)
+public abstract class CassandraClientTestBase {
+
+  private static final int NATIVE_TRANSPORT_PORT = 9142;
+
+  private final AtomicReference<Context> capturedContext = new AtomicReference<>();
+
+  protected Vertx vertx = Vertx.vertx();
+  protected CQLDataLoader cqlDataLoader = new CQLDataLoader(EmbeddedCassandraServerHelper.getSession());
+  protected CassandraClient client;
+
+  @BeforeClass
+  public static void startEmbeddedCassandra() throws Exception {
+    EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+  }
+
+  @Before
+  public void setUp() {
+    client = CassandraClient.createNonShared(vertx, createClientOptions());
+  }
+
+  @After
+  public void tearDown(TestContext testContext) {
+    if (client != null) {
+      client.close(testContext.asyncAssertSuccess());
+    }
+    if (vertx != null) {
+      vertx.close(testContext.asyncAssertSuccess());
+    }
+    EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
+  }
+
+  protected CassandraClientOptions createClientOptions() {
+    return new CassandraClientOptions().setPort(NATIVE_TRANSPORT_PORT);
+  }
+
+  protected void initializeRandomStringKeyspace(int rowsPerLetter) {
+    cqlDataLoader.load(new RandomStringsDataSet(rowsPerLetter));
+  }
+
+  protected void initializeNamesKeyspace() {
+    cqlDataLoader.load(new NamesDataSet());
+  }
+
+  protected void checkContext(TestContext testContext) {
+    Context context = vertx.getOrCreateContext();
+    if (capturedContext.compareAndSet(null, context)) {
+      context.exceptionHandler(testContext::fail);
+    } else {
+      testContext.assertEquals(capturedContext.get(), context);
+    }
+  }
+
+  protected static void getCassandraReleaseVersion(CassandraClient client, Handler<AsyncResult<String>> handler) {
+    client.executeWithFullFetch("select release_version from system.local", ar -> {
+      if (ar.succeeded()) {
+        List<Row> result = ar.result();
+        handler.handle(Future.succeededFuture(result.iterator().next().getString("release_version")));
+      } else {
+        handler.handle(Future.failedFuture(ar.cause()));
+      }
+    });
+  }
+
+  private static class RandomStringsDataSet implements CQLDataSet {
+
+    final int rowsPerLetter;
+
+    RandomStringsDataSet(int rowsPerLetter) {
+      this.rowsPerLetter = rowsPerLetter;
+    }
+
+    @Override
+    public List<String> getCQLStatements() {
+      Stream.Builder<String> builder = Stream.builder();
+      builder.add("create table random_strings.random_string_by_first_letter (first_letter text, random_string text, primary key (first_letter, random_string));");
+      for (char c = 'A'; c <= 'Z'; c++) {
+        for (int i = 0; i < rowsPerLetter; i++) {
+          String randomString = UUID.randomUUID().toString();
+          String statement = String.format("INSERT INTO random_strings.random_string_by_first_letter (first_letter, random_string) VALUES ('%s', '%s')", c, c + randomString);
+          builder.add(statement);
+        }
+      }
+      return builder.build().collect(toList());
+    }
+
+    @Override
+    public String getKeyspaceName() {
+      return "random_strings";
+    }
+
+    @Override
+    public boolean isKeyspaceCreation() {
+      return true;
+    }
+
+    @Override
+    public boolean isKeyspaceDeletion() {
+      return true;
+    }
+  }
+
+  private class NamesDataSet implements CQLDataSet {
+    @Override
+    public List<String> getCQLStatements() {
+      return Collections.singletonList("create table names.names_by_first_letter (first_letter text, name text, primary key (first_letter, name));");
+    }
+
+    @Override
+    public String getKeyspaceName() {
+      return "names";
+    }
+
+    @Override
+    public boolean isKeyspaceCreation() {
+      return true;
+    }
+
+    @Override
+    public boolean isKeyspaceDeletion() {
+      return true;
+    }
+  }
+}
--- src/test/java/io/vertx/cassandra/StreamingTest.java
+++ src/test/java/io/vertx/cassandra/StreamingTest.java
@@ -1,22 +1,23 @@
 /*
- * Copyright 2018 The Vert.x Community.
+ * Copyright 2019 Red Hat, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 package io.vertx.cassandra;
 
 import com.datastax.driver.core.Row;
-import io.vertx.core.Future;
+import com.datastax.driver.core.SimpleStatement;
+import com.datastax.driver.core.Statement;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -24,81 +25,55 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 @RunWith(VertxUnitRunner.class)
-public class StreamingTest extends CassandraServiceBase {
+public class StreamingTest extends CassandraClientTestBase {
 
   @Test
-  public void testReadStream(TestContext context) {
-    CassandraClient cassandraClient = CassandraClient.createNonShared(
-      vertx,
-      new CassandraClientOptions().setPort(NATIVE_TRANSPORT_PORT)
-    );
-    Async async = context.async();
-    Future<Void> future = Future.future();
-    cassandraClient.connect(future);
-    future.compose(connected -> {
-      Future<CassandraRowStream> queryResult = Future.future();
-      cassandraClient.queryStream("select random_string from random_strings.random_string_by_first_letter where first_letter = 'A'", queryResult);
-      return queryResult;
-    }).compose(stream -> {
-      List<Row> items = new ArrayList<>();
+  public void testReadStream(TestContext testContext) {
+    initializeRandomStringKeyspace(50);
+    String query = "select random_string from random_strings.random_string_by_first_letter where first_letter = 'A'";
+    Statement statement = new SimpleStatement(query)
+      .setFetchSize(5); // make sure data is not loaded at once from Cassandra
+    Async async = testContext.async();
+    client.queryStream(query, testContext.asyncAssertSuccess(stream -> {
+      List<Row> items = Collections.synchronizedList(new ArrayList<>());
       AtomicInteger idx = new AtomicInteger();
       long pause = 500;
       long start = System.nanoTime();
       stream.endHandler(end -> {
         long duration = NANOSECONDS.toMillis(System.nanoTime() - start);
-        context.assertTrue(duration >= 3 * pause);
-        cassandraClient.disconnect(disconnected -> async.countDown());
-      }).exceptionHandler(context::fail)
-        .handler(item -> {
-          items.add(item);
-          int j = idx.getAndIncrement();
-          if (j == 3 || j == 16 || j == 38) {
-            stream.pause();
-            int emitted = items.size();
-            vertx.setTimer(pause, tid -> {
-              context.assertTrue(emitted == items.size());
-              stream.resume();
-            });
-          }
-        });
-
-      return Future.succeededFuture();
-    }).setHandler(h -> {
-      if (h.failed()) {
-        context.fail(h.cause());
-      }
-    });
+        testContext.assertTrue(duration >= 5 * pause);
+        async.countDown();
+      }).exceptionHandler(testContext::fail).handler(item -> {
+        items.add(item);
+        int j = idx.getAndIncrement();
+        if (j == 3 || j == 16 || j == 21 || j == 38 || j == 47) {
+          stream.pause();
+          int emitted = items.size();
+          vertx.setTimer(pause, tid -> {
+            testContext.assertTrue(emitted == items.size());
+            stream.resume();
+          });
+        }
+      });
+    }));
   }
 
   @Test
-  public void emptyStream(TestContext context) {
-    CassandraClient cassandraClient = CassandraClient.createNonShared(
-      vertx,
-      new CassandraClientOptions().setPort(NATIVE_TRANSPORT_PORT)
-    );
-    Async async = context.async();
-    Future<Void> future = Future.future();
-    cassandraClient.connect(future);
-    future.compose(connected -> {
-      Future<CassandraRowStream> queryResult = Future.future();
-      cassandraClient.queryStream("select random_string from random_strings.random_string_by_first_letter where first_letter = 'I WANT EMPTY RESULT'", queryResult);
-      return queryResult;
-    }).compose(stream -> {
-      stream.endHandler(end -> cassandraClient.disconnect(disconnected -> async.countDown()))
-        .exceptionHandler(context::fail)
-        .handler(item -> context.fail());
-      return Future.succeededFuture();
-    }).setHandler(h -> {
-      if (h.failed()) {
-        context.fail(h.cause());
-      }
-    });
+  public void emptyStream(TestContext testContext) {
+    initializeRandomStringKeyspace(1);
+    String query = "select random_string from random_strings.random_string_by_first_letter where first_letter = '$'";
+    Async async = testContext.async();
+    client.queryStream(query, testContext.asyncAssertSuccess(stream -> {
+      stream.endHandler(end -> async.countDown())
+        .exceptionHandler(testContext::fail)
+        .handler(item -> testContext.fail());
+    }));
   }
-
 }
