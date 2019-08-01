b7cbd9c324f46d15b63196fe5bc71c9a163ab7a9
Sammers21 <titantins@gmail.com>
2019-08-01 23:25:08 +0300

    add a comment explaining when the statement can be removed

--- src/test/java/io/vertx/cassandra/CassandraClientTestBase.java
+++ src/test/java/io/vertx/cassandra/CassandraClientTestBase.java
@@ -57,6 +57,7 @@ public abstract class CassandraClientTestBase {
   @BeforeClass
   public static void startEmbeddedCassandra() throws Exception {
     String version = System.getProperty("java.version");
+    // this statement can be removed only when this issue[https://github.com/jsevellec/cassandra-unit/issues/249] will be resolved
     if (!version.startsWith("1.8")) {
       throw new IllegalStateException("Only Java 8 is allowed for running tests. Your java version: " + version);
     }
