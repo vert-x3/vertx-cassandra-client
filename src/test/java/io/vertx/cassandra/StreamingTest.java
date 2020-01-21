3cf917972c550c12a00fbcfadbb1558fa4e98d9b
Pavel Drankov <titantins@gmail.com>
2020-01-21 15:09:55 +0300

    Row stream overflow fix (#48)
    * implement a reproducer for #47
    
    * fix #47
    
    * change cassandra unit log level to INFO

--- src/test/java/io/vertx/cassandra/StreamingTest.java
+++ src/test/java/io/vertx/cassandra/StreamingTest.java
@@ -17,6 +17,7 @@ package io.vertx.cassandra;
 
 import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.core.cql.SimpleStatementBuilder;
 import com.datastax.oss.driver.api.core.cql.Statement;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -62,6 +63,24 @@ public class StreamingTest extends CassandraClientTestBase {
           });
         }
       });
+    }));
+  }
+
+  @Test
+  public void streamFetchesDoesNotOverflowDefault512KbJVMStack(TestContext testContext) {
+    int fetchSize = 100_000;
+    initializeRandomStringKeyspace(5_000);
+    final SimpleStatement query = new SimpleStatementBuilder(
+      String.format(
+        "select random_string from random_strings.random_string_by_first_letter limit %d",
+        fetchSize
+      )
+    ).setPageSize(fetchSize).build();
+    Async async = testContext.async();
+    client.queryStream(query, testContext.asyncAssertSuccess(stream -> {
+      stream.endHandler(end -> async.countDown())
+        .exceptionHandler(testContext::fail)
+        .handler(item -> {});
     }));
   }
 
